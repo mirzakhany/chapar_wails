d83094dc9afefb65daf4d8902a823f6f6aa8aee7
Mohsen Mirzakhani <mohsen.mkh88@gmail.com>
2024-01-06 19:56:12 +0100

    mac webview

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
@@ -27,6 +28,9 @@ func main() {
 		OnStartup:        app.startup,
 		Bind: []interface{}{
 			app,
+		},
+		Mac: &mac.Options{
+			WebviewIsTransparent: true,
 		},
 	})
 
